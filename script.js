644c77ecb484f65f10e2c2dfd31a71635139af1f
James Halliday <mail@substack.net>
2013-03-20 19:28:46 -0700

    wrong place to put the parens, should work now

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var assertion = 0;
 console.log('TAP version 13');
 console.log('1..3');
 
-equal(text(document.querySelector('#a'), 'A'));
-equal(text(document.querySelector('#b'), 'B'));
-equal(text(document.querySelector('#c'), 'C'));
+equal(text(document.querySelector('#a')), 'A');
+equal(text(document.querySelector('#b')), 'B');
+equal(text(document.querySelector('#c')), 'C');
 
 function text(elem) { return elem.textContent || elem.innerText }
 function equal (a, b) {
